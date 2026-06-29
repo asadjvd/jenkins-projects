a6893a82e8058aa59a76e0f77f009eebfaf49805
Asad Javed <asad.jvd00@gmail.com>
2026-06-29 15:20:22 +0500

    Update Terraform backend configuration

--- EKS-TF-Jenkins/eks/backend.tf
+++ EKS-TF-Jenkins/eks/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.9.3"
+  required_version = "~> 1.15.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "dev-asadjvd-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
